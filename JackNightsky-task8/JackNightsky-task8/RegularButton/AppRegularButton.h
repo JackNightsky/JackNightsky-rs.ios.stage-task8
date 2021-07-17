e07a01a2bb2be8acf65173cf4d26b845883d42b3
JackNigthsky <jack.shapyalevich@gmail.com>
2021-07-18 02:36:25 +0300

    feat: implement regular buttons to Artist screen and their custom class

---
+++ JackNightsky-task8/JackNightsky-task8/RegularButton/AppRegularButton.h
@@ -0,0 +1,16 @@
+//
+//  AppRegularButton.h
+//  JackNightsky-task8
+//
+//  Created by Jack on 17.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AppRegularButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JackNightsky-task8/JackNightsky-task8/RegularButton/AppRegularButton.m
@@ -0,0 +1,86 @@
+//
+//  AppRegularButton.m
+//  JackNightsky-task8
+//
+//  Created by Jack on 17.07.21.
+//
+
+#import "AppRegularButton.h"
+#import "ColorPalette.h"
+
+@implementation AppRegularButton
+
+
+// Set appearens for states Checked and Unchecked
+-(void)setHighlighted:(BOOL)highlighted {
+    [super setHighlighted:highlighted];
+    if (highlighted) {
+        self.layer.backgroundColor = [[UIColor rsWhite] colorWithAlphaComponent:1].CGColor;
+        
+        self.layer.shadowColor = [UIColor.rsLightGreenSea colorWithAlphaComponent:1].CGColor;
+        self.layer.shadowRadius  = 4;
+    } else {
+        [self commonInit];
+    }
+}
+
+
+// Set appearens for states Enabled and Disabled
+- (void)setEnabled:(BOOL)enabled {
+    [super setEnabled:enabled];
+    if (!enabled) {
+        self.layer.shadowOpacity = 0.25;
+        self.alpha = 0.5;
+    } else {
+        [self commonInit];
+    }
+}
+
+
+- (void) commonInit {
+    // crutch
+    if (!self.isEnabled) {
+        self.alpha = 0.5;
+    }
+    
+    self.titleLabel.font = [UIFont fontWithName:@"Montserrat-Medium" size:18];
+    
+    // Button appearance for state NORMAL
+    self.backgroundColor = UIColor.rsWhite;
+    
+    [self setTitleColor:UIColor.rsLightGreenSea forState:UIControlStateNormal];
+    
+    self.layer.cornerRadius  = 10;
+    
+    self.layer.shadowColor   = [UIColor.rsBlack colorWithAlphaComponent:0.25].CGColor;
+    self.layer.shadowRadius  = 2;
+    self.layer.shadowOpacity = 0.5;
+    self.layer.shadowOffset  = CGSizeMake(0, 0);
+    
+    [self setTitleEdgeInsets:UIEdgeInsetsMake(5, 21, 5, 21)];
+}
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+     self = [super initWithCoder:aDecoder];
+     if (self) {
+         [self commonInit];
+         
+         // Set title color for states
+         [self setTitleColor:UIColor.rsLightGreenSea forState:UIControlStateNormal];
+         [self setTitleColor:[UIColor.rsLightGreenSea colorWithAlphaComponent:1] forState:UIControlStateHighlighted];
+         [self setTitleColor:[UIColor.rsLightGreenSea colorWithAlphaComponent:1] forState:UIControlStateDisabled];
+     }
+     return self;
+ }
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
