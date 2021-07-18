d1e1a3f5aa0d7129427f3910c5bcf39bdfbaadc2
JackNigthsky <jack.shapyalevich@gmail.com>
2021-07-18 23:09:43 +0300

    add svg template as code presentation on Objective-C

---
+++ Assets/templates_to_draw/Their presentation as code/head_in_objc.m
@@ -0,0 +1,119 @@
+
+
+
+
+
+
+
+// path0
+UIBezierPath * path0 = [UIBezierPath bezierPath];
+[path0 moveToPoint:CGPointMake(81.5, 49)];
+[path0 addLineToPoint:CGPointMake(97, 109)];
+[path0 addLineToPoint:CGPointMake(109, 132)];
+[path0 addLineToPoint:CGPointMake(126.5, 151.5)];
+[path0 addLineToPoint:CGPointMake(153.5, 174)];
+[path0 addLineToPoint:CGPointMake(177, 179.5)];
+[path0 addLineToPoint:CGPointMake(213, 162)];
+[path0 addLineToPoint:CGPointMake(240, 132)];
+[path0 addLineToPoint:CGPointMake(248.5, 120)];
+[path0 addLineToPoint:CGPointMake(250.5, 70.5)];
+[path0 addLineToPoint:CGPointMake(238.5, 60.5)];
+[path0 addLineToPoint:CGPointMake(222, 63.5)];
+[path0 addLineToPoint:CGPointMake(211, 80.5)];
+[path0 addLineToPoint:CGPointMake(209, 103.5)];
+[path0 addLineToPoint:CGPointMake(213, 116)];
+
+path0.lineWidth = 1;
+[UIColor.blackColor setStroke];
+[path0 stroke];
+
+// path1
+UIBezierPath * path1 = [UIBezierPath bezierPath];
+[path1 moveToPoint:CGPointMake(204, 120)];
+[path1 addLineToPoint:CGPointMake(195.5, 118.5)];
+[path1 addLineToPoint:CGPointMake(186, 120.5)];
+[path1 addLineToPoint:CGPointMake(178, 121.5)];
+[path1 addLineToPoint:CGPointMake(168.5, 120.5)];
+[path1 addLineToPoint:CGPointMake(160, 119)];
+[path1 addLineToPoint:CGPointMake(153.5, 118.5)];
+[path1 addLineToPoint:CGPointMake(146, 120)];
+[path1 addLineToPoint:CGPointMake(141.5, 122)];
+[path1 addLineToPoint:CGPointMake(147.5, 124.5)];
+[path1 addLineToPoint:CGPointMake(152, 128)];
+[path1 addLineToPoint:CGPointMake(156.5, 133)];
+[path1 addLineToPoint:CGPointMake(162.5, 135.5)];
+[path1 addLineToPoint:CGPointMake(170, 136.5)];
+[path1 addLineToPoint:CGPointMake(177, 135.5)];
+[path1 addLineToPoint:CGPointMake(184.5, 136.5)];
+[path1 addLineToPoint:CGPointMake(190.5, 135.5)];
+[path1 addLineToPoint:CGPointMake(197, 131.5)];
+[path1 addLineToPoint:CGPointMake(204, 123.5)];
+[path1 addLineToPoint:CGPointMake(208.5, 117.5)];
+[path1 addLineToPoint:CGPointMake(200.5, 116.5)];
+[path1 addLineToPoint:CGPointMake(191.5, 115.5)];
+[path1 addLineToPoint:CGPointMake(182.5, 113.5)];
+[path1 addLineToPoint:CGPointMake(174, 113)];
+[path1 addLineToPoint:CGPointMake(164, 114.5)];
+[path1 addLineToPoint:CGPointMake(155, 116.5)];
+[path1 addLineToPoint:CGPointMake(145, 117.5)];
+[path1 addLineToPoint:CGPointMake(138, 117)];
+[path1 addLineToPoint:CGPointMake(147.5, 111)];
+[path1 addLineToPoint:CGPointMake(156.5, 104.5)];
+[path1 addLineToPoint:CGPointMake(162.5, 101)];
+[path1 addLineToPoint:CGPointMake(167.5, 102.5)];
+[path1 addLineToPoint:CGPointMake(173, 104.5)];
+[path1 addLineToPoint:CGPointMake(179.5, 103.5)];
+[path1 addLineToPoint:CGPointMake(186, 102.5)];
+[path1 addLineToPoint:CGPointMake(194.5, 104.5)];
+[path1 addLineToPoint:CGPointMake(199.5, 109.5)];
+[path1 addLineToPoint:CGPointMake(207, 114)];
+
+path1.lineWidth = 1;
+[UIColor.blackColor setStroke];
+[path1 stroke];
+
+// path2
+UIBezierPath * path2 = [UIBezierPath bezierPath];
+
+[path2 moveToPoint:CGPointMake(209.5, 122.5)];
+[path2 addLineToPoint:CGPointMake(214, 128.5)];
+[path2 addLineToPoint:CGPointMake(216.5, 135)];
+[path2 addLineToPoint:CGPointMake(213, 144)];
+[path2 addLineToPoint:CGPointMake(206, 152.5)];
+[path2 addLineToPoint:CGPointMake(197, 159.5)];
+[path2 addLineToPoint:CGPointMake(187.5, 152.5)];
+[path2 addLineToPoint:CGPointMake(177, 148.5)];
+[path2 addLineToPoint:CGPointMake(155.5, 152.5)];
+[path2 addLineToPoint:CGPointMake(147.5, 162)];
+[path2 addLineToPoint:CGPointMake(141, 174.5)];
+[path2 addLineToPoint:CGPointMake(129.5, 167.5)];
+[path2 addLineToPoint:CGPointMake(121.5, 157.5)];
+[path2 addLineToPoint:CGPointMake(113, 148.5)];
+[path2 addLineToPoint:CGPointMake(106, 219)];
+[path2 addLineToPoint:CGPointMake(94.5, 227.5)];
+[path2 addLineToPoint:CGPointMake(83.5, 234.5)];
+[path2 addLineToPoint:CGPointMake(101, 241)];
+[path2 addLineToPoint:CGPointMake(114.5, 249.5)];
+[path2 addLineToPoint:CGPointMake(125, 263.5)];
+[path2 addLineToPoint:CGPointMake(139, 281)];
+[path2 addLineToPoint:CGPointMake(158, 299)];
+[path2 addLineToPoint:CGPointMake(177, 305.5)];
+[path2 addLineToPoint:CGPointMake(206, 297.5)];
+[path2 addLineToPoint:CGPointMake(219.5, 281)];
+[path2 addLineToPoint:CGPointMake(229.5, 259.5)];
+[path2 addLineToPoint:CGPointMake(239, 243.5)];
+[path2 addLineToPoint:CGPointMake(253.5, 237)];
+[path2 addLineToPoint:CGPointMake(250.5, 221.5)];
+[path2 addLineToPoint:CGPointMake(241, 193)];
+[path2 addLineToPoint:CGPointMake(239, 170)];
+[path2 addLineToPoint:CGPointMake(232, 157.5)];
+[path2 addLineToPoint:CGPointMake(224, 165.5)];
+[path2 addLineToPoint:CGPointMake(216.5, 174.5)];
+[path2 addLineToPoint:CGPointMake(200, 190.5)];
+[path2 addLineToPoint:CGPointMake(190, 205)];
+[path2 addLineToPoint:CGPointMake(181.5, 226.5)];
+[path2 addLineToPoint:CGPointMake(178.5, 252.5)];
+
+path2.lineWidth = 1;
+[UIColor.blackColor setStroke];
+[path2 stroke];
