dc5ba72278e090adcc556c680b16b87dc82bfca5
JackNigthsky <jack.shapyalevich@gmail.com>
2021-07-20 19:51:20 +0300

    feat: implement slider from timer get value

--- JackNightsky-task8/JackNightsky-task8/TimerView/TimerViewController.swift
+++ JackNightsky-task8/JackNightsky-task8/TimerView/TimerViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 class TimerViewController: UIViewController {
-
+    @IBOutlet var currentDurationLabel: UILabel!
+    @IBOutlet var durationSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,9 +18,15 @@ class TimerViewController: UIViewController {
     }
     
     @IBAction func saveChoiseAndCloseVC(_ sender: Any) {
+        PlistWorker.writeValue(forKey:"drawDuration", withValue: round(durationSlider.value * 100))
         self.dismiss(animated: true, completion:nil)
     }
     
+    @IBAction func sliderMove(_ sender: Any) {
+//        print("durationSlider.value", durationSlider.value)
+        let currentDurationRounded = round(durationSlider.value * 100) / 100
+        currentDurationLabel.text = String("\(currentDurationRounded) s")
+    }
     /*
     // MARK: - Navigation
 
