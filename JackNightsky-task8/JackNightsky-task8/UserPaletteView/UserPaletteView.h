ffd660806f589d3404d29699215438aac5d0c873
JackNigthsky <jack.shapyalevich@gmail.com>
2021-07-20 06:44:06 +0300

    feat: add button save to userPaletee

---
+++ JackNightsky-task8/JackNightsky-task8/UserPaletteView/UserPaletteView.h
@@ -0,0 +1,16 @@
+//
+//  UserPaletteView.h
+//  JackNightsky-task8
+//
+//  Created by Jack on 20.07.21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UserPaletteView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JackNightsky-task8/JackNightsky-task8/UserPaletteView/UserPaletteView.m
@@ -0,0 +1,43 @@
+//
+//  UserPaletteView.m
+//  JackNightsky-task8
+//
+//  Created by Jack on 20.07.21.
+//
+
+#import "UserPaletteView.h"
+#import "ColorPalette.h"
+
+@implementation UserPaletteView
+
+- (void) commonInit {
+    self.backgroundColor = UIColor.rsWhite;
+//    self.backgroundColor = UIColor.rsShrimple;
+    
+    self.layer.cornerRadius  = 40;
+    
+    self.layer.shadowColor   = [UIColor.rsBlack colorWithAlphaComponent:0.5].CGColor;
+    self.layer.shadowRadius  = 8;
+    self.layer.shadowOpacity = 1;
+    self.layer.shadowOffset  = CGSizeMake(0, 0);
+}
+
+
+- (id)initWithCoder:(NSCoder *)aDecoder {
+     self = [super initWithCoder:aDecoder];
+     if (self) {
+         [self commonInit];
+     }
+     return self;
+ }
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
